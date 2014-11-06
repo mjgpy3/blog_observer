1d05513ef3541eaa8e7b9cf6f432685b5a5782af
Michael Gilliland <mjg.py3@gmail.com>
2014-11-05 19:54:39 -0500

    Refactors

--- lib/blog_observer.rb
+++ lib/blog_observer.rb
@@ -1,13 +1,15 @@
 class BlogObserver
   def initialize(params)
-    @config = params[:config]
-    @analyzer = params[:update_analyzer]
-    @emailer = params[:email_sender]
+    @config, @analyzer, @emailer = params.values_at(:config, :update_analyzer, :email_sender)
   end
 
   def observe
-    ->(updates) {
-      @emailer.send_updates(updates) unless updates.empty?
-    }.(@analyzer.updates(@config.blogs))
+    @emailer.send_updates(updates) unless updates.empty?
+  end
+
+  private
+
+  def updates
+    @updates ||= @analyzer.updates(@config.blogs)
   end
 end
