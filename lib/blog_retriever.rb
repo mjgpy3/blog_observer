a2d0e51383de404f8f0c989e6c7b3fb0c3dae6d2
Michael Gilliland <mjg.py3@gmail.com>
2014-11-06 00:11:50 -0500

    A little more work on the retriever

--- lib/blog_retriever.rb
+++ lib/blog_retriever.rb
@@ -1,11 +1,14 @@
 require 'net/http'
 require 'nokogiri'
 
-#@doc = Nokogiri::XML(File.open("shows.xml"))
-#@doc.xpath("//character")
-
 class BlogRetriever
+  def initialize(get = Net::HTTP.method(:get), to_xml = Nokogiri.method(:XML))
+    @get = get
+    @to_xml = to_xml
+  end
+
   def retrieve(blog_details)
-    Nokogiri::XML(Net::HTTP.get(blog_details['link']))
+    xpath = blog_details['title_xpath']
+    @to_xml.(@get.(blog_details['link'])).xpath(xpath)
   end
 end
--- spec/lib/blog_retriever_spec.rb
+++ spec/lib/blog_retriever_spec.rb
@@ -8,7 +8,9 @@ describe BlogRetriever do
     subject { blog_retriever.retrieve(blog_details) }
 
     context 'when provided blog details' do
-      let(:blog_details) { { 'link' => 'foo.bar.foo' } }
+      let(:blog_details) { { 'link' => 'foo.bar.foo', 'title_xpath' => 'some xpath' } }
+      before(:each) { allow(Net::HTTP).to receive(:get).and_return(double.as_null_object) }
+      before(:each) { allow(Nokogiri).to receive(:XML).and_return(double.as_null_object) }
 
       it "gets the blog's HTML by its link" do
         expect(Net::HTTP).to receive(:get).with(blog_details['link'])
@@ -22,6 +24,16 @@ describe BlogRetriever do
         it 'creates a Nokogiri::XML document from it' do
           expect(Nokogiri).to receive(:XML).with(html)
           subject
+        end
+
+        describe 'the Nokogiri::XML document' do
+          let(:nokogiri_document) { double('Nokogiri::XML') }
+          before(:each) { allow(Nokogiri).to receive(:XML).and_return(nokogiri_document) }
+
+          it "gets the blog's xpath applied to it" do
+            expect(nokogiri_document).to receive(:xpath).with(blog_details['title_xpath'])
+            subject
+          end
         end
       end
     end
